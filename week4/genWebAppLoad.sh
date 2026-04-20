daeffa15f7ae7de2823b68cf082bade725ff45a6
zensaiello <stvaiello@gmail.com>
2026-04-20 14:40:38 -0500

    (4-2)

--- week4/genWebAppLoad.sh
+++ week4/genWebAppLoad.sh
@@ -2,9 +2,13 @@
 
 hitWebApp() {
   date
-  for i in {1..15}; do
+  for i in {1..$((5 + RANDOM % 15))} ; do
     curl -s http://localhost:5000/orders > /dev/null &
+  done
+  for i in {1..$((5 + RANDOM % 15))} ; do
     curl -s http://localhost:5000/orders/$((RANDOM % 5 + 1)) > /dev/null &
+  done
+  for i in {1..$((5 + RANDOM % 15))} ; do
     curl -s -X POST http://localhost:5000/orders/checkout > /dev/null &
   done
 }
